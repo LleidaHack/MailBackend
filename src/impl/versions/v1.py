fd47895b6ba77936dbef2a57a5014269098e0839
tonlls <tonlls1999@gmail.com>
2024-05-23 22:36:54 +0200

    fix things

--- src/impl/versions/v1.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-from src.impl.Mail.router import router as mail_router
-from src.impl.Template.router import router as template_router
-
-router = APIRouter(prefix="/v1", )
-router.include_router(mail_router)
-router.include_router(template_router)
