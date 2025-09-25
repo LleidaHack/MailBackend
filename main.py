eece4d2eb5299acd38a8546e70d1ec84aa3dcd92
Lotfi Bouakel <95545807+Big-Lolo@users.noreply.github.com>
2025-09-25 10:08:09 +0200

    Debugging

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from fastapi_utils.tasks import repeat_every
 from src.configuration.Settings import settings
 from src.impl.Mail.service import MailService
 from src.versions.v1 import router as v1_router
+import sys
 
 mail_service = MailService()
 
@@ -31,13 +32,16 @@ for route in app.routes:
             route.tags) > 0 else ''
         route.operation_id += '_' + route.name
 
-
+print("PRINT DE PRUEBA: Esto es un print para gestionar funcionamiento de los logs :)", file=sys.stderr)
 @app.on_event("startup")
 @repeat_every(seconds=60 * 60 * 24)
 def send_pending_mails():
+    print("send_pending_mails: Entrada en función.", file=sys.stderr)
     while True:
         try:
+            print("send_pending_mails: INTENTO DE ENVIO", file=sys.stderr)
             mail_service.send_next()
             sleep(60)
         except Exception:
+            print("send_pending_mails: ERROR EN EL INTENTO", file=sys.stderr)
             break
