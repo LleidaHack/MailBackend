6416204d18a92beadd9506628411c31644f62007
Ton <47422572+tonlls@users.noreply.github.com>
2025-09-25 13:08:17 +0200

    Update Settings.py

--- src/configuration/Settings.py
+++ src/configuration/Settings.py
@@ -35,6 +35,11 @@ import os
 
 
 class DatabaseSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env",
+                                      env_file_encoding="utf-8",
+                                      env_nested_delimiter="__",
+                                      case_sensitive=False,
+                                      extra="ignore")
     url: str = Field(...,
                      description="Database connection URL",
                      alias=AliasChoices("DATABASE__URL"))
